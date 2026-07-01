9546996815f88602d5898ff3d5f70354a910619b
MohammadMustafa23 <ff774823@gmail.com>
2026-07-01 22:42:47 +0530

    Fixing Bugs And Finish Backend

--- server/src/services/gemini.service.js
+++ server/src/services/gemini.service.js
@@ -4,7 +4,7 @@ import { GEMINI_API_KEY } from "../config/env.js";
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-3-flash-preview",
+  model: "gemini-2.5-flash",
 });
 
 export async function generateSearchQuery(profile) {
