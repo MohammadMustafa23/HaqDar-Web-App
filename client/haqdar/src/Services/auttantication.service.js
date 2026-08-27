32d9297172f3b234e5b1a1b24e456b0094c68656
MohammadMustafa23 <ff774823@gmail.com>
2026-08-27 15:41:51 +0530

    Fixing Backned URL Chnage server

--- client/haqdar/src/Services/auttantication.service.js
+++ client/haqdar/src/Services/auttantication.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.BACKNED_URL,
   withCredentials: true,
   timeout: 20000,
 });
